d509d4ff1f791a6f7e05442739a4dc37d10f27a3
Metin <mahiskali@outlook.com>
2018-08-01 13:47:00 +0000

    This time fibo.py has text in it. Oops

---
+++ fibo.py
@@ -0,0 +1,15 @@
+# Fibonacci Numbers Module
+def fib(n): # print out Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n): # return array of Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
